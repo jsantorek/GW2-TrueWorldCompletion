841e38977fa8c62c1201ac51852ce4e0faa45063
Jakub Santorek <jakub.santorek@gmail.com>
2025-02-20 08:57:02 +0100

    incremented https id limit

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -4,5 +4,5 @@ namespace TWC
 {
 static constexpr auto MapProgressWidgetWidthMultiplier = 1.2f;
 static constexpr auto ConfigFilename = "config.json";
-static constexpr auto HttpsMaxMapIdCount = 20;
+static constexpr auto HttpsMaxMapIdCount = 200;
 } // namespace TWC
